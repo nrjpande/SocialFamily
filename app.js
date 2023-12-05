58167b248437f2a04828388869eda84b03acf343
nrjpande <nrjpandey63@gmail.com>
2023-12-05 21:40:36 +0530

    fourth commit to remove gitignore file from merny-social

--- app.js
+++ app.js
@@ -25,11 +25,11 @@ mongoose.connection.on("error", () => {
   console.log("Not Connected to mongodb");
 });
 
-app.use(express.static(path.join(__dirname, "./merny-social/build")));
+app.use(express.static(path.join(__dirname, "./frontend/build")));
 
 app.get("*", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "./merny-social/build/index.html"),
+    path.join(__dirname, "./frontend/build/index.html"),
     function (err) {
       res.status(500).send(err);
     }
